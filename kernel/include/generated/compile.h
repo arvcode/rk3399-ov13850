2821ce5b6f0288fdee14ad6d23d4227650b65627
arvcode <s.arvindh@gmail.com>
2018-10-27 22:56:29 -0700

    rk3399 ov13850

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 51 */
+/* SMP */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#51 SMP Sat Oct 27 22:18:17 PDT 2018"
+#define LINUX_COMPILE_BY "arvindh"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 5.4.0 20160609 (Ubuntu/Linaro 5.4.0-6ubuntu1~16.04.9) "
